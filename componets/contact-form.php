dfde873d1dd4e047994075396786cce7b1d49289
Randy James <rjames@webdev.com>
2021-10-15 00:47:41 -0600

    added google map

---
+++ componets/contact-form.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>  
+<title>The Sammy Center</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+  
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+  />
+  
+  </head>
+    <body>
+        <!-- Container (Contact Section) -->
+    <div class="w3-content w3-container w3-padding-64" id="contact">
+      <h3 class="w3-center">GET INVOLVED!</h3>
+      <p class="w3-center"><em>We'd love your help or insight!</em></p>
+
+      <div class="w3-row w3-padding-32 w3-section">
+        <div class="w3-col m4 w3-container">
+        <iframe width="300" height="300" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJWc0_PddjUocRJUIzKHu13FY&key=AIzaSyC_MgsYzuFy5HUxE6WDZU1bxTIZszKaads"></iframe>
+        </div>
+        <div class="w3-col m8 w3-panel">
+          <div class="w3-large w3-margin-bottom">
+            <i
+              class="
+                fa fa-map-marker fa-fw
+                w3-hover-text-black w3-xlarge w3-margin-right
+              "
+            ></i>
+            Chicago, US<br />
+            <i
+              class="
+                fa fa-phone fa-fw
+                w3-hover-text-black w3-xlarge w3-margin-right
+              "
+            ></i>
+            Phone: +00 151515<br />
+            <i
+              class="
+                fa fa-envelope fa-fw
+                w3-hover-text-black w3-xlarge w3-margin-right
+              "
+            ></i>
+            Email: mail@mail.com<br />
+          </div>
+          <p>
+            Swing by for a cup of <i class="fa fa-coffee"></i>, or leave me a
+            note:
+          </p>
+          <form action="/action_page.php" target="_blank">
+            <div class="w3-row-padding" style="margin: 0 -16px 8px -16px">
+              <div class="w3-half">
+                <input
+                  class="w3-input w3-border"
+                  type="text"
+                  placeholder="Name"
+                  required
+                  name="Name"
+                />
+              </div>
+              <div class="w3-half">
+                <input
+                  class="w3-input w3-border"
+                  type="text"
+                  placeholder="Email"
+                  required
+                  name="Email"
+                />
+              </div>
+            </div>
+            <input
+              class="w3-input w3-border"
+              type="text"
+              placeholder="Message"
+              required
+              name="Message"
+            />
+            <button
+              class="w3-button w3-black w3-right w3-section"
+              type="submit"
+            >
+              <i class="fa fa-paper-plane"></i> SEND MESSAGE
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+    </body>
+</html>
