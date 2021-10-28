9352258c11b891a6f2c743a4ed68c2b631a7ada2
rjdev <rjdev@dev.com>
2021-10-28 17:41:45 -0600

    fixed slider

--- componets/contact-form.php
+++ componets/contact-form.php
@@ -18,132 +18,19 @@ function test_input($data)
   return $data;
 }
 
-?>
+echo $fName;
+echo $lName;
+echo $email;
+echo $message;
 
-
-<!DOCTYPE html>
-<html>
-
-<head>
-  <title>Thank you</title>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
-  <link rel="stylesheet" href="../style/main.css" />
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato" />
-  <link rel="stylesheet" href="../style/rstyle.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-  <script src="../src/main.js" defer></script>
-
-</head>
-
-<body>
-  <!-- Navbar (sit on top) -->
-  <div class="w3-top">
-    <div class="w3-bar" id="myNavbar">
-      <a class="
-            w3-bar-item
-            w3-button
-            w3-hover-black
-            w3-hide-medium
-            w3-hide-large
-            w3-right
-          " href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
-        <i class="fa fa-bars"></i>
-      </a>
-      <a href="#home" class="w3-bar-item w3-button">HOME</a>
-      <a href="#about" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
-      <a href="#portfolio" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-th"></i> BOARD</a>
-      <a href="#contact" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-envelope"></i> CONTACT</a>
-      <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-red">
-        <i class="fa fa-search"></i>
-      </a>
-    </div>
-
-    <!-- Navbar on small screens -->
-    <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
-      <a href="#about" class="w3-bar-item w3-button" onclick="toggleFunction()">ABOUT</a>
-      <a href="#portfolio" class="w3-bar-item w3-button" onclick="toggleFunction()">PORTFOLIO</a>
-      <a href="#contact" class="w3-bar-item w3-button" onclick="toggleFunction()">CONTACT</a>
-      <a href="#" class="w3-bar-item w3-button">SEARCH</a>
-    </div>
-  </div>
-
-  <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
-    <div class="w3-display-middle" style="white-space: nowrap">
-      <span class="
-            w3-center
-            w3-padding-large
-            w3-black
-            w3-xlarge
-            w3-wide
-            w3-animate-opacity
-          ">THANK YOU
-
-    </div>
-  </div>
-
-
-  <div class="w3-container w3-section">
-    <ul>
-      <li><?php echo $fName ?></li>
-      <li><?php echo $lName ?></li>
-      <li><?php echo $email ?></li>
-      <li><?php echo $message ?></li>
-      <?php
-
-      $ahelp = $_POST["help"];
+$ahelp = $_POST["help"];
       $n = count($ahelp);
 
 
       for ($i = 0; $i < $n; $i++) {
-        echo "<li>" . $ahelp[$i] . "</li>";
+        echo $ahelp[$i];
       }
 
-      ?>
-    </ul>
-  </div>
 
-  <script>
-    // Modal Image Gallery
-    function onClick(element) {
-      document.getElementById("img01").src = element.src;
-      document.getElementById("modal01").style.display = "block";
-      var captionText = document.getElementById("caption");
-      captionText.innerHTML = element.alt;
-    }
-
-    // Change style of navbar on scroll
-    window.onscroll = function() {
-      myFunction();
-    };
-
-    function myFunction() {
-      var navbar = document.getElementById("myNavbar");
-      if (
-        document.body.scrollTop > 100 ||
-        document.documentElement.scrollTop > 100
-      ) {
-        navbar.className =
-          "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
-      } else {
-        navbar.className = navbar.className.replace(
-          " w3-card w3-animate-top w3-white",
-          ""
-        );
-      }
-    }
-
-    // Used to toggle the menu on small screens when clicking on the menu button
-    function toggleFunction() {
-      var x = document.getElementById("navDemo");
-      if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-      } else {
-        x.className = x.className.replace(" w3-show", "");
-      }
-    }
-  </script>
-</body>
-
-</html>
+?>
+  
--- src/main.js
+++ src/main.js
@@ -1,18 +1,61 @@
 var slideIndex = 1;
+showSlides(slideIndex);
 
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showDivs(n) {
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
+
+// Get contact form
+
+const contactForm = document.getElementById('contact-form');
+
+// onclick listener and stop page reload
+
+contactForm.addEventListener('submit', function(event){
+  event.preventDefault();
+  
+  const formattedFormData = new FormData(contactForm);
+  
+  postData(formattedFormData);
+});
+
+//POST data and return reponse
+
+async function postData(formattedFormData){
+
+  const response = await fetch('../componets/contact-form.php',{
+    method: 'POST',
+    body: formattedFormData
+  });
+
+  const data = await response.text();
+  console.log(data)
+
+  const thankYouMsg = document.getElementById('thank-you');
+
+  contactForm.style.display = "none";
+
+  thankYouMsg.classList.remove('w3-hide');
+
+}
+
